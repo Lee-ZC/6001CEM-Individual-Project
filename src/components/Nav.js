13c0cf7c7cfa51be032dbb3a360cfbe0e1f53c6b
Lee-ZC <leezc0631@gmail.com>
2023-09-16 15:03:33 +0800

    Login&SignUp UI

---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+export default function NavBar(){
+    return (
+    
+    <nav className="nav">
+        <a href="/" className="site-title"> HealthHub</a>
+
+        <ul>
+            <li>
+                <a href="/pricing">Pricing</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+        </ul>
+
+
+
+
+    </nav>
+    )
+
+}
